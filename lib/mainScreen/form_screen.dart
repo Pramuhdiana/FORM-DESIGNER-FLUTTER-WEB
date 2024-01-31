1c653e509bd0981c0a6ce4b784e2783db497470d
pramuhdiana <sandypramuhdiana96@gmail.com>
2024-01-31 16:46:20 +0700

    clear error

--- lib/mainScreen/form_screen.dart
+++ lib/mainScreen/form_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: sized_box_for_whitespace, avoid_print, depend_on_referenced_packages
+// ignore_for_file: sized_box_for_whitespace, avoid_print, depend_on_referenced_packages, use_build_context_synchronously
 import 'dart:developer';
 
 import 'dart:convert';
